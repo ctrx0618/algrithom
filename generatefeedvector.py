bd47ca3a55f824bfd2ba0f66c39198d54f2bb6aa
ctrx0618 <ctrx0618@163.com>
2017-07-19 00:51:32 +0800

    添加数据获取代码。
    待解决：
    徐有效的英文RSS源

--- generatefeedvector.py
+++ generatefeedvector.py
@@ -16,13 +16,12 @@ def getwords(html):
 
 
 def get_word_count(url):
-
     # 解析订阅源
     d = feedparser.parse(url)
     wc = {}
 
     for e in d.entries:
-        if 'summary' in e :
+        if 'summary' in e:
             summary = e.summary
         else:
             summary = e.description
@@ -33,3 +32,36 @@ def get_word_count(url):
             wc[word] += 1
 
     return d.feed.title, wc
+
+
+if __name__ == '__main__':
+    apcount = {}
+    wordcounts = {}
+    feedlist = [line for line in file('feedlist.txt')]
+    for feedurl in feedlist:
+        title, wc = get_word_count(feedurl)
+        wordcounts[title] = wc
+        for word, count in wc.items():
+            apcount.setdefault(word, 0)
+            if count > 1:
+                apcount[word] += 1
+
+    wordlist = []
+    for w, bc in apcount.items():
+        frac = float(bc) / len(feedlist)
+        if frac > 0.1 and frac < 0.5:
+            wordlist.append(w)
+
+    out = file('blogdata.txt', 'w')
+    out.write('Blog')
+    for word in wordlist:
+        out.write("\t%s" % word)
+    out.write('\n')
+    for blog, wc in wordcounts.items():
+        out.write(blog)
+        for word in wordlist:
+            if word in wc:
+                out.write('\t%d' % wc[word])
+            else:
+                out.write('\t0')
+        out.write('\n')
