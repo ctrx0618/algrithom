c564537a02a6dd026f77d2b9e7f692306c018173
ctrx0618 <ctrx0618@163.com>
2017-07-19 00:05:20 +0800

    完成单词计数

--- generatefeedvector.py
+++ generatefeedvector.py
@@ -4,6 +4,17 @@ import feedparser
 import re
 
 
+def getwords(html):
+    # 去掉HTML标记
+    txt = re.compile(r'<[^>]+>').sub('', html)
+
+    # 利用所有非字母字符拆分出单词
+    words = re.compile(r'[^A-Z^a-z]+').split(txt)
+
+    # 转化成小写
+    return [word.lower() for word in words if word != '']
+
+
 def get_word_count(url):
 
     # 解析订阅源
@@ -15,3 +26,10 @@ def get_word_count(url):
             summary = e.summary
         else:
             summary = e.description
+
+        words = getwords(e.title + ' ' + summary)
+        for word in words:
+            wc.setdefault(word, 0)
+            wc[word] += 1
+
+    return d.feed.title, wc
